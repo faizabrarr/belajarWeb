2c5267b2ef7915b356e9f4f1eeea84c7db622489
gabisatidur <faizabrar112@gmail.com>
2022-11-29 10:05:38 +0700

    pwdGen

---
+++ 8. WEB Password Generator/index.js
@@ -0,0 +1,26 @@
+let passwordLength = document.getElementById("passwordLength")
+let password = document.getElementById("password")
+
+function generatePassword(len) {
+    const lowerAlphabet = "abcdefghijklmmopqrstuvwxyz"
+    const upperAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    const numneric = "0123456789"
+    const symbol = "~!@#$%^&*()_+-={}|[];',<>?./:`"
+
+    const data = lowerAlphabet + upperAlphabet + numneric + symbol
+    let generator = '';
+    for (let index = 0; index < len; index++) {
+        generator += data[~~(Math.random() * data.length)];        
+    }
+    return generator
+}
+
+function getPassword() {
+    const newPassword = generatePassword(passwordLength.value)
+    password.value = newPassword
+}
+
+function savePassword() {
+    saveButton.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(`password saya: ${document.title}`))
+    saveButton.setAttribute('download', 'MyPasswordGeneratorLOG.txt')
+}
